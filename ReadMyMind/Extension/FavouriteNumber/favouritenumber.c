00d54ce702687b0560df2410cd6265a33233af72
Justin Yang <justinyang28@googlemail.com>
2018-03-15 15:26:02 +0000

    implemented extensions

--- ReadMyMind/Extension/FavouriteNumber/favouritenumber.c
+++ ReadMyMind/Extension/FavouriteNumber/favouritenumber.c
@@ -18,14 +18,13 @@ Deck* createDeck(Deck* deck, int no_of_cards, int suits, int ranks) {
             free(card_pointer);
         }
     }
-
     return deck;
 }
 
 //method to print contents of deck
 void printDeck(Deck* deck, int rows, int columns) {
     Card* temp = deck->front;
-    printf("%d\t%d\t%d\n", 1, 2, 3);
+    printf("\n%d\t%d\t%d\n", 1, 2, 3);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
             printCard(temp);
@@ -53,36 +52,104 @@ int getUserInput() {
     return user_input;   
 }
 
-//method to rotate decks
-Deck* combineDecks(Deck* deck, Deck* deck_to_middle, Deck* first_deck_to_rotate, Deck* second_deck_to_rotate) {
+//method to get a number from the user between 0 and 27
+int getUserNumber() {
+    int user_input;
+    char initial_input[100];
+    printf("Enter a number between 0 and 27: ");
+    while(1) {
+        scanf("%s", initial_input);
+        if (sscanf(initial_input,"%d", &user_input)) {
+            if (user_input > 0 && user_input < 27) {
+                break;
+            }
+        }
+        printf("Invalid input! Enter a number between 0 and 27: ");
+    }
+    return user_input;   
+}
+
+//method which converts a number into base three
+void convertToBaseThree(int* base_three_array, int user_number) {
+    int index = 0;
+    int base = 3;
+    while (user_number != 0) {
+        base_three_array[index] = user_number % base;
+        user_number = user_number / base;
+        ++index;
+    }
+}
+
+
+//method to combine decks
+Deck* combineDecks(Deck* deck, Deck* top_deck, Deck* middle_deck, Deck* bottom_deck) {
     int i = 0;
-    int size_of_deck = getSize(first_deck_to_rotate);
+    int size_of_deck = getSize(top_deck);
     
     //dequeue all elements in column deck and enqueue it into the main deck
     while (i < size_of_deck) {
-        enqueue(dequeue(first_deck_to_rotate), deck);
+        enqueue(dequeue(top_deck), deck);
         i++;
     }
 
     i = 0;
     //dequeue all elements in column deck and enqueue it into the main deck
     while (i < size_of_deck) {
-        enqueue(dequeue(deck_to_middle), deck);
+        enqueue(dequeue(middle_deck), deck);
         i++;
     }
 
     i = 0;
     //dequeue all elements in column deck and enqueue it into the main deck
     while (i < size_of_deck) {
-        enqueue(dequeue(second_deck_to_rotate), deck);
+        enqueue(dequeue(bottom_deck), deck);
         i++;
     }
 
     return deck;
 }
 
+Deck* switchDecks(int position, Deck* deck, Deck* deck_to_be_switched, Deck* other_deck, Deck* deck_other) {
+    
+    switch (position) {
+        //put user selected deck to top of pile
+        case 0:
+            deck = combineDecks(deck, deck_to_be_switched, other_deck, deck_other);
+            break;
+        //put user selected deck to middle of pile
+        case 1:
+            deck = combineDecks(deck, deck_other, deck_to_be_switched, other_deck);
+            break;
+        //put user selected deck to bottom of pile
+        case 2:
+            deck = combineDecks(deck, other_deck, deck_other, deck_to_be_switched);
+            break;
+    }
+
+    return deck;
+}
+
+//method to position the decks
+Deck* positionDecks(int user_input, int position, Deck* deck, Deck* deck_one, Deck* deck_two, Deck* deck_three) {
+    
+    //get the user selected deck to switch
+    switch (user_input) {
+        case 1:
+            deck = switchDecks(position, deck, deck_one, deck_two, deck_three);
+            break;
+        case 2:
+            deck = switchDecks(position, deck, deck_two, deck_one, deck_three);
+            break;
+        case 3:
+            deck = switchDecks(position, deck, deck_three, deck_one, deck_two);
+            break;
+    }
+
+    return deck;
+}
+
 //method to collect the cards
-Deck* collectDeck(Deck* deck, int user_input) {
+Deck* collectDeck(Deck* deck, int user_input, int position) {
 
     //three decks
     Deck* deck_one = malloc(sizeof(Deck));
@@ -118,23 +185,8 @@ Deck* collectDeck(Deck* deck, int user_input) {
     deck->front = NULL;
     deck->rear = NULL;
 
-    //put user deck in the middle
-    switch(user_input) {
-        //column 1 goes to the middle
-        case 1:
-            deck = combineDecks(deck, deck_one, deck_two, deck_three);
-            break;
-
-        //column 2 goes to the middle
-        case 2:
-            deck = combineDecks(deck, deck_two, deck_one, deck_three);
-            break;
-
-        //column 3 goes to the middle
-        case 3:
-            deck = combineDecks(deck, deck_three, deck_two, deck_one);
-            break;
-    }
+    //position the decks
+    deck = positionDecks(user_input, position, deck, deck_one, deck_two, deck_three);
 
     //free the decks
     free(deck_one);
@@ -147,16 +199,22 @@ Deck* collectDeck(Deck* deck, int user_input) {
 
 //main method
 int main() {
-
-    int no_of_rows = 7;
     int no_of_columns = 3;
+    int no_of_rows = 9;
     int no_of_cards = no_of_columns * no_of_rows;
     int no_of_suits = 4;
     int no_of_ranks = 13;
     int no_of_repitions = 3;
-    int prediction = no_of_cards / 2;
+    int user_number, user_input;
+    int base_three_array[3]; 
 
-    //create deck of 21 random cards
+    //pick a number between 0 and 27
+    user_number = getUserNumber();
+
+    //convert number into base three
+    convertToBaseThree(base_three_array, user_number);
+
+    //create deck of 27 random cards
     Deck* deck_pointer = malloc(sizeof(Deck));
     deck_pointer->front = NULL;
     deck_pointer->rear = NULL;
@@ -165,8 +223,8 @@ int main() {
 
     //display the cards
     printDeck(deck_pointer, no_of_rows, no_of_columns);
-    int user_input;
-    //the trick
+
+    //pick a card shown in one of the columns
     for (int i = 0; i < no_of_repitions; i++) {
         //get input
         user_input = getUserInput();
@@ -178,14 +236,16 @@ int main() {
             return 0;
         }
         //collect deck
-        deck_pointer = collectDeck(deck_pointer, user_input);
+        deck_pointer = collectDeck(deck_pointer, user_input, base_three_array[i]);
         //print deck
         printDeck(deck_pointer, no_of_rows, no_of_columns);
     }
 
-    //make prediction
-    Card* predicted_card = getCard(deck_pointer, prediction);
-    printf("I predict your card to be: ");
+    //user's card is in the position the user chose
+    Card* predicted_card = getCard(deck_pointer, user_number);
+    printf("At the start I said to choose a number, and you chose %d\n", user_number);
+    printf("Amazingly, through shuffling and manipulating the decks, I have put your card in that position\n");
+    printf("Hence, I predict your card to be: ");
     printCard(predicted_card);
     printf("\n");
 
