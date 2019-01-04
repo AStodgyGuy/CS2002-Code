b0cd9fbe5deb50438acbdacf2c7b7064c606fcda
AStodgyGuy <jy50@palain.cs.st-andrews.ac.uk>
2019-01-04 11:37:58 +0000

    adding files

--- Tutorial/test.c
+++ Tutorial/test.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-	int pixel = 2;
-	printf("pixel before bitwise operator = %d\n", pixel);
-	pixel ^= ~0;
-	printf("pixel after bitwise operator = %d\n", pixel);
+	typedef struct test1{int i; char c;} test1;
+	typedef union test2{char d; int i; char c;} test2;
+	printf("%d\n", sizeof(test1));
+	printf("%d\n", sizeof(test2));
 }
